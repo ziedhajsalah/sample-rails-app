c550f475e916c67cf093494fb722446e3ee4a4cf
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2016-10-12 11:52:05 +0100

    add destroy action with tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
 99.times do |n|
   User.create!(
     name: Faker::Name.name,
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,6 +6,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   # end
   def setup
    @user = users(:zied)
+   @other_user = users(:zizou)
   end
 
   test 'index include pagination' do
@@ -13,8 +14,34 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
+
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
+  end
+
+  test 'index as admin including pagination and delete links' do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page: 1)
+    
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @user
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+    
+    assert_difference 'User.count', -1 do
+      delete user_path(@other_user)
+    end
+  end
+
+  test 'index as non-admin' do
+    log_in_as(@other_user)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
